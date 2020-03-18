fa757dc356bbbe1ca28b18fd6502a51c4bf13596
julh974 <darcksky974@gmail.com>
2020-03-18 11:38:34 +0400

    Initial Commit

---
+++ Securité Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Securité Info
+//
+//  Created by HOARAU Julien on 18/03/2020.
+//  Copyright © 2020 HOARAU Julien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
